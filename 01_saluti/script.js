57e57efebbd3047408ccaf2f9f3aef3d79ed22d8
defendimattia <defendi.mattia92@gmail.com>
2025-03-03 15:29:59 +0100

    add: snack1 solution

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,10 +1,24 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
+const userName = 'Mario';
 
-// Dichiara la funzione qui.
 
+// // Dichiara la funzione qui.
+// function toGreet(name) {
+//     const greetName = "ciao " + name;
+//     return greetName;
+// };
+
+// ARROW FUNCTION
+
+const toGreet = (name) => {
+    const greetName = "ciao " + name;
+    return greetName;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const helloName = toGreet(userName);
+console.log(helloName);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
